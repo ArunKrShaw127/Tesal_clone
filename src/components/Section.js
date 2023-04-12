8033a9ef5dcda91ebfc4f19bd80cbf461f81163b
ArunKrShaw127 <71167301+ArunKrShaw127@users.noreply.github.com>
2023-04-12 20:55:45 +0530

    Update Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -44,12 +44,12 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image: url('images/model-s.jpg');
+    background-image: url('./images/model-s.jpg');
     display: flex;
     flex-direction: column;
     justify-content: space-between; // Vertical
     align-items: center; // horizontal
-    background-image: ${props => `url("images/${props.bgImage}")`};
+    background-image: ${props => `url("./images/${props.bgImage}")`};
 
 
 `
